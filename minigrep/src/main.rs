71a1cca707730bf7fdb3ae57165c5aed5f089eac
incomprehensible <inc0mprehensible@protonmail.com>
2020-04-07 07:38:10 +0300

    more practice with command line

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {} in {}", config.query, config.file);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Could not read contents of the file: {}", e);
+        process::exit(1);
+    }
+}
